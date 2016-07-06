91b7825aef5566d1cd9490d7cc1930764f9baa43
Cem <jimcc333@gmail.com>
2016-07-06 13:19:11 -0500

    adds PCA solver

--- src/main.py
+++ src/main.py
@@ -22,40 +22,45 @@ import os
 from objects import *
 from parser import *
 
+import numpy as np
+from matplotlib.mlab import PCA as mlabPCA
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 def main(args):
 	print "hello world"
 	
 	databasepath = "/home/cem/nudge/db_dbtest1/"
-	libraries = 0
+	database = objects.DBase("database1")
 	
+	libraries = 0
 	for filename in os.listdir(databasepath):
-		
 		if filename[0:5] == "build": 
 			libraries += 1
-			inputfile = databasepath + filename + "/brightlite0/922350.txt"
-			
-			try:
-				doc = open(inputfile, "r")
-				
-			except IOError:
-				print "Could not open ", inputfile
-				libraries -= 1
-				continue
-			
-			for line in doc.readlines():
-				items = line.split()
-				# print items, " ", items[0]
-				if items[0] == "NEUT_PROD":
-					print "detected!"
-		break
+			lib = objects.Library(databasepath + filename, libraries)
+			database.Add(lib)
 	
 	print "Total libraries: ", libraries
+	database.UpdateData()	
+	database.PCA()
 	
+	pca_mat = mlabPCA(database.data_mat)
 	
-	lib1 = objects.Library(path = "path", number = 1)
+	fig1 = plt.figure()
+	ax1 = fig1.add_subplot(111, projection='3d')
+	ax1.scatter(database.data_mat[:,0], database.data_mat[:,1], database.data_mat[:,2])
+	ax1.set_xlabel("Neutron Production")
+	ax1.set_xlabel("Neutron Destruction")
+	ax1.set_zlabel("Burnup")
 	
-	print "blabla"
-	print lib1.fuel_cell_radius
+	fig2 = plt.figure()
+	ax2 = fig2.add_subplot(111, projection='3d')
+	ax2.scatter(pca_mat.Y[:,0], pca_mat.Y[:,1], pca_mat.Y[:,2])
+	ax2.set_xlabel("PC1")
+	ax2.set_xlabel("PC2")
+	ax2.set_zlabel("PC3")
+	plt.show()
 	
 	return 0
 
--- src/objects.py
+++ src/objects.py
@@ -1,9 +1,41 @@
+import numpy as np
+
 class Library:
 	""" A class that holds library information """ 
 	
 	def __init__(self, path, number):
-		self.path = path		# Path of the library folder
+		self.path = path		# Path of the library folder w/ brightlite0
 		self.number = number	# Unique number of the library
+		
+		self.max_prod = 0
+		self.max_dest = 0
+		self.max_BU = 0
+		
+		#TODO: pass combining fractions (frac) better
+		
+		u235_file = path + "/brightlite0/922350.txt"
+		self.Read("U235", u235_file, 0.04)
+		
+		u238_file = path + "/brightlite0/922380.txt"
+		self.Read("U235", u238_file, 0.96)
+	
+		
+	def Read(self, nuclide, file_path, frac):	
+		try:
+			doc = open(file_path, "r")
+		except IOError:
+			print "Could not open ", file_path
+					
+		for line in doc.readlines():
+			items = line.split()
+			
+			if items[0] == "NEUT_PROD":
+				self.max_prod += float(items[len(items)-1]) * frac
+			if items[0] == "NEUT_DEST":
+				self.max_dest += float(items[len(items)-1]) * frac
+			if items[0] == "BUd":
+				self.max_BU += sum( [float(i) for i in items[1:]] ) * frac
+		doc.close()
 	
 	# --- Inputs ---
 	# Initial heavy metal mass fraction distribution
@@ -36,10 +68,54 @@ class Library:
 	
 class DBase:
 	""" A class that handles all generated libraries """
+	libs = []
+	max_prods = []
+	max_dests = []
+	max_BUs = []
+	
+	def __init__(self, name):
+		self.name = name
+		
 	# Database specific immutable parameters
 	
+	
 	# stored libraries
+	def Add(self, added_lib):
+		self.libs.append(added_lib)
 	
-	# stored information about current database
+	# information about current database
+	def UpdateData(self):
+		for i in self.libs:
+			self.max_prods.append(i.max_prod)
+			self.max_dests.append(i.max_dest)
+			self.max_BUs.append(i.max_BU)
+		
+	def PCA(self):
+		self.np_prods = np.asarray(self.max_prods)
+		self.np_dests = np.asarray(self.max_dests)
+		self.np_BUs = np.asarray(self.max_BUs)
+		
+		self.data_mat = np.column_stack((self.np_prods, self.np_dests, self.np_BUs))
+		
+		
+		
 	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
