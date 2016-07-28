1a92c7f0afa557de1dc1d42d01e4549c345de9b6
Cem <jimcc333@gmail.com>
2016-07-28 13:23:53 -0500

    adds the placeholder xsgen software file

---
+++ src/pxsgen.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+#
+#  
+#  Copyright 2016 Cem Bagdatli
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  Naming 
+
+from objects import *
+import os
+
+import numpy as np
+
+
+def main(args):
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print('Placeholder xsgen program')
+	
+	
+	
+	
+	print('\n-TheEnd-')	
+	return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
