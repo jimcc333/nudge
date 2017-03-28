f596ac77ff4f1827c9e6dfae31f56612b3e3db57
hephaestus <william.gurecky@gmail.com>
2017-03-28 18:20:15 -0500

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(name='nudge',
+      version='0.0.1',
+      description='Nuclear data generation software.',
+      author='Cem Bagdatlioglu',
+      packages=find_packages(),
+      test_suite="tests",
+      install_requires=['numpy>=1.8.0', 'scipy', 'matplotlib'],
+      package_data={'': ['*.txt']},
+      license='GPL-3',
+      author_email='cem@cem-VirtualB',
+      entry_points={
+          'console_scripts': [
+              'nudge = src:main.main'
+          ]
+      }
+)
